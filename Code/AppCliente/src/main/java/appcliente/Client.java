6982061010008b225a4c8cd628d74fe9bdd151b7
AtlasVIM <caldasmartim@gmail.com>
2024-10-26 17:14:46 +0100

    implementaçao dos contratos de cliente

--- Code/AppCliente/src/main/java/appcliente/Client.java
+++ Code/AppCliente/src/main/java/appcliente/Client.java
@@ -1,17 +1,22 @@
 package appcliente;
 
-import calcstubs.CalcServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import primeclientstubs.Number;
+import ringmanagerclientstubs.*;
+import primeclientstubs.*;
 
 import java.util.Scanner;
 
 public class Client {
     private static String clientIP = "localhost";
     private static int clientPort = 8500;
+
+
     private static ManagedChannel channel;
-    private static CalcServiceGrpc.CalcServiceBlockingStub blockingStub;
-    private static CalcServiceGrpc.CalcServiceStub noBlockStub;
+    private static RingManagerClientServiceGrpc.RingManagerClientServiceBlockingStub ringBlockStub;
+    private static PrimeClientServiceGrpc.PrimeClientServiceBlockingStub primeServerBlockStub;
+
     public static void main(String[] args) {
         try {
             if (args.length == 2) {
@@ -23,19 +28,39 @@ public class Client {
                     .usePlaintext()
                     .build();
 
+            ringBlockStub = RingManagerClientServiceGrpc.newBlockingStub(channel);
+
             while (true) {
                 switch (Menu()) {
                     case 0:
                         System.exit(0);
                     case 1:
-                        //prime;
+                        checkPrimality();
                     case 2:
-                        //server info
+                        PrimeServerAddress address = ringBlockStub.getPrimeServer(VoidRequest.newBuilder()
+                                .build());
+
+                        System.out.println("Server available in " + address.getIp() + " in port " + address.getPort());
                 }
 
             }
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    private static void checkPrimality() {
+        System.out.println("Enter a number to check if it's prime: ");
+        Scanner scanner = new Scanner(System.in);
+        long num = scanner.nextLong();
+
+        Number req = Number.newBuilder().setNumber(num).build();
+
+        try {
+            PrimalityResult res = primeServerBlockStub.isPrime(req);
+            System.out.println("The number " + num + " is " + (res.getIsPrime() ? "prime!" : "not prime!"));
+        } catch (Exception e) {
+            System.err.println("AN ERROR HAS OCCURRED WHILE CHECKING PRIMALITY: " +e.getMessage());
         }
     }
 
@@ -47,7 +72,7 @@ public class Client {
             System.out.println("  ---  MENU  ---  ");
             System.out.println("0 - EXIT SERVER");
             System.out.println("1 - PRIME NUMBER CLASSIFIER");
-            System.out.println("2 - SERVER INFO");
+            System.out.println("2 - REQUEST SERVER");
             num = scan.nextInt();
         } while (!(num == 0));
         return num;
